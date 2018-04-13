6eb6d232bb0e2252a03b1f3c2dac86fdfec236ae
fmadio <aaron@fmad.io>
2018-04-14 00:30:00 +0900

    updated with basic CnC interfaces, list + get. basic functionality is now complete

--- main.c
+++ main.c
@@ -21,17 +21,38 @@
 #include <arpa/inet.h>
 #include <errno.h>
 
-
 //-------------------------------------------------------------------------------------------
+
+#define PACKETHEADER_FLAG_EOF			(1<<0)	// end of capture
 
 typedef struct
 {
 	u32                 SeqNo;              // chunk seq no
 	u32                 XferLength;         // byte length of transfer size
 	u32                 DataLength;         // raw unpacked data length
-	u32                 CRC32;              // chunks crc32
+	u8                 	Flag;           	// flags for the chunk 
+	u8					pad[3];
 
 } __attribute__((packed)) PacketHeader_t;
+
+
+#define CMDHEADER_CMD_LIST          1       // list all the captures
+#define CMDHEADER_CMD_GET           2       // get a capture
+#define CMDHEADER_CMD_END          100 		// end of communications 
+#define CMDHEADER_CMD_OK           101 		// sucess 
+#define CMDHEADER_CMD_NG           102 		// failed 
+
+typedef struct
+{
+	u32                 Cmd;                // command to issue
+	u8                  StreamName[1024];	// stream info 
+	u64					StreamSize;
+
+	u8                  FilterBPF[1024];    // run BPF filter
+	u8                  FilterRE[1024];     // run RegEx filter
+
+} __attribute__((packed)) CmdHeader_t;
+
 
 // standard PCAP header 
 
@@ -53,6 +74,32 @@ typedef struct
 	u32             Link;
 
 } __attribute__((packed)) PCAPHeader_t;
+
+typedef struct PCAPPacket_t
+{
+	u32             Sec;                    // time stamp sec since epoch
+	u32             NSec;                   // nsec fraction since epoch
+
+	u32             LengthCapture;			// captured length
+	u32             LengthWire;				// Length on the wire 
+
+} __attribute__((packed)) PCAPPacket_t;
+
+
+// internal format thats on the tcp connection
+// contains some extra metadata
+typedef struct FMADPacket_t
+{
+	u64             TS;                     // 64bit nanosecond epoch 
+
+	u32             LengthCapture	: 16;	// length captured 
+	u32             LengthWire		: 16;   // Length on the wire 
+
+	u32             PortNo			:  8;   // Port number 
+	u32             pad1			:  8;   // flags 
+	u32             pad0			: 16; 
+
+} __attribute__((packed)) FMADPacket_t;
 
 
 typedef struct Chunk_t
@@ -112,6 +159,8 @@ volatile u32 g_Exit = false;
 
 static volatile u32			s_ChunkFreeLock[128/4];		// use a full 128B cache line to avoid contention
 static volatile Chunk_t*	s_ChunkFree	= NULL;			// free chunk list
+
+static volatile u32			s_EOFSeqNo = 0;				// indicates SeqNo for EOF
 
 //-------------------------------------------------------------------------------------------
 // light weight mutexs
@@ -202,7 +251,6 @@ static Network_t* NetworkOpen(u32 CPUID, u32 PortBase)
 	N->Buffer 		= memalign2(128, 256*1024);
 	N->BufferMax 	= 256*1024;
 
-
 	// reset queue
 	N->Queue.Put	= 0;
 	N->Queue.Get	= 0;
@@ -213,15 +261,43 @@ static Network_t* NetworkOpen(u32 CPUID, u32 PortBase)
 
 //-------------------------------------------------------------------------------------------
 
+static bool RecvSock(int Sock, u8* Buffer8, s32 BufferLength)
+{
+	while (BufferLength > 0)
+	{
+		int rlen = recv(Sock, Buffer8, BufferLength, 0);
+		if (rlen <= 0)
+		{
+			if (errno != EAGAIN)
+			{
+				fprintf(stderr, "RecvSock Error %i %i %s\n", rlen, errno, strerror(errno));
+				return false;	
+			}
+		}
+		else
+		{
+			// iterate thought the raw buffer 
+			BufferLength	-= rlen;
+			Buffer8			+= rlen;	
+		}
+	}
+	return true;
+}
+
+//-------------------------------------------------------------------------------------------
+// process a single TCP condition and push the result on the output queu
 void* RxThread(void* _User)
 {
 	Network_t* N = (Network_t*)_User;
-	printf("[%i] RxThread starting\n", N->CPUID);
+	fprintf(stderr, "[%i] RxThread starting\n", N->CPUID);
 
 	// receive at maximum rate per thread 
 	bool Exit = false;
 	while (!Exit)
 	{
+		// global exit
+		if (g_Exit) break;
+
 		// dont have too many outstanding entries
 		// Put/Get are 64b and reset for each download
 		// its impossible to for this to wrap around 
@@ -242,6 +318,13 @@ void* RxThread(void* _User)
 		// get the packet header first
 		s32 HeaderLength 	= sizeof(PacketHeader_t);
 		u8* Header8			= (u8*)&C->Header;
+		if(!RecvSock(N->Sock, Header8, HeaderLength))
+		{
+			Exit = true;
+			fprintf(stderr, "recv failed %s\n", strerror(errno));
+			break;
+		}
+		/*
 		while (HeaderLength > 0)
 		{
 			int rlen = recv(N->Sock, Header8, HeaderLength, 0);
@@ -252,10 +335,21 @@ void* RxThread(void* _User)
 			}
 			if ((rlen < 0) && (errno != EAGAIN))
 			{
-				fprintf(stderr, "recv failed %s\n", strerror(errno));
 				Exit = true;
 				break;
 			}
+		}
+		*/
+
+		// check for End of File marker
+		if (C->Header.Flag & PACKETHEADER_FLAG_EOF)
+		{
+			fprintf(stderr, "EOF Reached SeqNo: %i\n", C->Header.SeqNo);
+			if (C->Header.SeqNo != 0)
+			{
+				s_EOFSeqNo		= C->Header.SeqNo;
+			}
+			break;
 		}
 
 		//printf("[%i] SeqNo: %i XferLen:%i %08x\n", PortNo, Header.SeqNo, Header.XferLength, Header.CRC32);
@@ -264,6 +358,17 @@ void* RxThread(void* _User)
 
 		s32 BufferLength= C->Header.XferLength;
 		u8* Buffer8		= (u8*)C->Data;
+		if(!RecvSock(N->Sock, Buffer8, BufferLength))
+		{
+			Exit = true;
+			fprintf(stderr, "recv data failed %s\n", strerror(errno));
+			break;
+		}
+
+		// stats 
+		N->TotalByte 	+= BufferLength;
+
+		/*
 		while (BufferLength > 0)
 		{
 			int rlen = recv(N->Sock, Buffer8, BufferLength, 0);
@@ -285,6 +390,35 @@ void* RxThread(void* _User)
 				Buffer8			+= rlen;	
 			}
 		}
+		*/
+
+		// mask out any meta data to make a pure PCAP 
+		u8* Data8 = (u8*)C->Data;	
+		u8* Data8End = Data8 + C->Header.DataLength; 
+		while (Data8 < Data8End)
+		{
+			FMADPacket_t* FPkt 	= (FMADPacket_t*)Data8;
+			PCAPPacket_t* PPkt 	= (PCAPPacket_t*)FPkt;
+
+			// convert from fmad packet to pcap packet
+			u64 TS				= FPkt->TS;
+			u32 LengthCapture 	= FPkt->LengthCapture;
+			u32 LengthWire 		= FPkt->LengthWire;
+
+			u32 PortNo			= FPkt->PortNo;
+
+			//
+			// *** here is where any custom filter logic goes ***
+			//
+
+			// overwrite
+			PPkt->Sec			= TS / 1e9;
+			PPkt->NSec			= TS - (u64)PPkt->Sec * 1000000000ULL;		// carefull keep full 64bit precision..
+			PPkt->LengthCapture	= LengthCapture;
+			PPkt->LengthWire	= LengthWire;
+
+			Data8 += sizeof(PCAPPacket_t) + PPkt->LengthCapture;
+		}
 
 		N->TotalChunk++;
 		N->LastSeqNo	= C->Header.SeqNo;
@@ -299,35 +433,24 @@ void* RxThread(void* _User)
 		N->Queue.Put++;
 	}
 
-	g_Exit = true;
+	fprintf(stderr, "[%i] RxThread exit\n", N->CPUID);
 
 	return NULL;
 }
 
 //-------------------------------------------------------------------------------------------
-
-int main(int argc, char* argv[])
+// master control thread that takes blocks recevied by each thread and
+// re-assemables them in order 
+static void GetStreamData(void)
 {
-	fprintf(stderr, "fmadio rsync: %s\n", __DATE__);
-
-	u8* CaptureName = NULL;	
-	for (int i=0; i < argc; i++)
-	{
-		// set the download capture name
-		if (argv[i][0] != '-')
-		{
-			CaptureName = argv[i];
-		}
-	}
-
 	CycleCalibration();
 
 	// init network connections	
 	Network_t* N[4];
-	N[0] = NetworkOpen(0, 10000);
-	N[1] = NetworkOpen(1, 10000);
-	N[2] = NetworkOpen(2, 10000);
-	N[3] = NetworkOpen(3, 10000);
+	N[0] = NetworkOpen(0, 10010);
+	N[1] = NetworkOpen(1, 10010);
+	N[2] = NetworkOpen(2, 10010);
+	N[3] = NetworkOpen(3, 10010);
 
 	// init the locks
 	s_ChunkFreeLock[0] = 1;
@@ -341,7 +464,7 @@ int main(int argc, char* argv[])
 	PCAPHeader.Minor	= PCAPHEADER_MINOR;
 	PCAPHeader.TimeZone	= 0; 
 	PCAPHeader.SigFlag	= 0; 
-	PCAPHeader.SnapLen	= 0; 
+	PCAPHeader.SnapLen	= 65535; 
 	PCAPHeader.Link		= PCAPHEADER_LINK_ETHERNET;
 	fwrite(&PCAPHeader, 1, sizeof(PCAPHeader), stdout);
 
@@ -395,6 +518,42 @@ int main(int argc, char* argv[])
 
 	while (!g_Exit)
 	{
+		// print some stats
+		u64 TSC = rdtsc();
+		if (TSC > NextPrintTSC)
+		{
+			NextPrintTSC = TSC + ns2tsc(1e9);	
+
+			double dByte = TotalByte - LastByte;
+			double dT = tsc2ns(TSC - LastTSC) / 1e9;
+			double bps = dByte * 8.0 / dT;
+
+			fprintf(stderr, "Recved %8.3f GB %8.3f Gbps Queue (%3i) (%3i) (%3i) (%3i)  : SeqNo: %i %i\n", 
+					TotalByte / 1e9, 
+					bps / 1e9,
+
+					(u32)(N[0]->Queue.Put - N[0]->Queue.Get),
+					(u32)(N[1]->Queue.Put - N[1]->Queue.Get),
+					(u32)(N[2]->Queue.Put - N[2]->Queue.Get),
+					(u32)(N[3]->Queue.Put - N[3]->Queue.Get),
+
+					SeqNo, s_EOFSeqNo
+			); 
+
+			LastByte 	= TotalByte;
+			LastTSC		= TSC;
+		}
+
+		// EOF ? 
+		// NOTE: SeqNo is the NEXT expected SeqNo not
+		//       last processed so it will match s_EOFSeqNo
+		if ((s_EOFSeqNo != 0) && (SeqNo == s_EOFSeqNo))
+		{
+			fprintf(stderr, "Last Chunk Written\n");
+			g_Exit = true;
+			break;
+		}
+
 		// find next seq no 
 		for (int c=0; c < 4; c++)
 		{
@@ -414,36 +573,13 @@ int main(int argc, char* argv[])
 				SeqNo 		= C->SeqNo + 1;
 
 				// write sequential data to disk 
-				fwrite(C->Data, 1, C->Header.DataLength, stdout);
+				int wlen = fwrite(C->Data, 1, C->Header.DataLength, stdout);
+				assert(wlen == C->Header.DataLength);
 
 				// recycle the chunk
 				ChunkFree(C);
 				Q->Get++;
 			}
-		}
-
-		// print some stats
-		u64 TSC = rdtsc();
-		if (TSC > NextPrintTSC)
-		{
-			NextPrintTSC = TSC + ns2tsc(1e9);	
-
-			double dByte = TotalByte - LastByte;
-			double dT = tsc2ns(TSC - LastTSC) / 1e9;
-			double bps = dByte * 8.0 / dT;
-
-			fprintf(stderr, "Recved %8.3f GB %8.3f Gbps Queue (%3i) (%3i) (%3i) (%3i)\n", 
-					TotalByte / 1e9, 
-					bps / 1e9,
-
-					(u32)(N[0]->Queue.Put - N[0]->Queue.Get),
-					(u32)(N[1]->Queue.Put - N[1]->Queue.Get),
-					(u32)(N[2]->Queue.Put - N[2]->Queue.Get),
-					(u32)(N[3]->Queue.Put - N[3]->Queue.Get)
-			); 
-
-			LastByte 	= TotalByte;
-			LastTSC		= TSC;
 		}
 	}
 
@@ -451,4 +587,104 @@ int main(int argc, char* argv[])
 	pthread_join(RxThread1, NULL);
 	pthread_join(RxThread2, NULL);
 	pthread_join(RxThread3, NULL);
+}
+
+//-------------------------------------------------------------------------------------------
+// list all streams on the device
+static void ListStreams(void)
+{
+	CycleCalibration();
+
+	Network_t* CnC = NetworkOpen(0, 10000);
+	assert(CnC != NULL);
+
+
+	CmdHeader_t Cmd;
+	memset(&Cmd, 0, sizeof(Cmd));
+	Cmd.Cmd	= CMDHEADER_CMD_LIST;         
+
+	// send request
+	send(CnC->Sock, &Cmd, sizeof(Cmd), 0);
+
+	// header
+	printf("%-60s | Capture Size\n", "Stream Name");
+	printf("-------------------------------------------------------------+------------------\n");
+
+	// wait for reposonse
+	while (true)
+	{
+		// get each stream 
+		if (!RecvSock(CnC->Sock, (u8*)&Cmd, sizeof(Cmd))) break;
+
+		// list finished
+		if (Cmd.Cmd == CMDHEADER_CMD_END) break;
+
+		printf("%-60s | %10.3f GB\n", Cmd.StreamName, Cmd.StreamSize / 1e9);
+	}
+	printf("-------------------------------------------------------------+------------------\n");
+	printf("\n");
+}
+
+//-------------------------------------------------------------------------------------------
+// get a specific stream 
+static void GetStream(u8* StreamName)
+{
+	CycleCalibration();
+
+	Network_t* CnC = NetworkOpen(0, 10000);
+	assert(CnC != NULL);
+
+	CmdHeader_t Cmd;
+	memset(&Cmd, 0, sizeof(Cmd));
+	Cmd.Cmd	= CMDHEADER_CMD_GET;         
+	strncpy(Cmd.StreamName, StreamName, sizeof(Cmd.StreamName));
+
+	// send request
+	send(CnC->Sock, &Cmd, sizeof(Cmd), 0);
+
+	// wait for reposonse
+	if (!RecvSock(CnC->Sock, (u8*)&Cmd, sizeof(Cmd)))
+	{
+		fprintf(stderr, "Failed to connect [%s]\n", StreamName);
+		return;
+	}
+
+	// check resposne
+	if (Cmd.Cmd != CMDHEADER_CMD_OK)
+	{
+		fprintf(stderr, "Failed to find stream [%s]\n", StreamName);
+		return;
+	}
+
+	// download it
+	GetStreamData();
+
+	// close CnC
+	shutdown(CnC->Sock, 0);
+}
+
+//-------------------------------------------------------------------------------------------
+
+int main(int argc, char* argv[])
+{
+	fprintf(stderr, "fmadio rsync: %s\n", __DATE__);
+
+	for (int i=1; i < argc; i++)
+	{
+		// set the download capture name
+		if (strcmp(argv[i], "--list") == 0)
+		{
+			ListStreams();
+		}
+		// featch a file and output to stdout 
+		else if (strcmp(argv[i], "--get") == 0)
+		{
+			GetStream(argv[i + 1]);
+			i++;
+		}
+		else
+		{
+			fprintf(stderr, "unknown command [%s]\n", argv[i]);
+		}
+	}
 }
